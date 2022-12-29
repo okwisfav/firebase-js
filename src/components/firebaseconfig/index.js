22f565a1c0c90442080a9f98f00998e444d06e2e
Oghenekohworo Oghenefego <ofegofavour@gmail.com>
2022-12-29 17:41:17 +0100

    Update index.js
    Update index.js

--- src/components/firebaseconfig/index.js
+++ src/components/firebaseconfig/index.js
@@ -4,13 +4,13 @@ import { getDatabase } from "firebase/database";
 function StartFirebase(){
    
 const firebaseConfig = {
-    apiKey: "AIzaSyC1AzGhGjS5m0PrKpg_bz7wcb3-WpGzgzc",
-    authDomain: "uneleap-9b858.firebaseapp.com",
-    databaseURL: "https://uneleap-ambassadors.firebaseio.com/",
-    projectId: "uneleap-9b858",
-    storageBucket: "uneleap-9b858.appspot.com",
-    messagingSenderId: "399698159769",
-    appId: "1:399698159769:web:32628b69175e083542b008",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "3959769",
+    appId: "",
     measurementId: "G-M0DL0DJRJG"
   };
 
